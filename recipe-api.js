de63242ba22ab7574f9dbd1e13f0c6c1581a2248
Maria Loriana Porumb <e1429844@student.tuwien.ac.at>
2021-04-11 10:27:24 +0200

    Use a proxy to add CORS headers to the request

--- recipe-api.js
+++ recipe-api.js
@@ -22,7 +22,7 @@ export async function retrieve(ingredients) {
 
 function api_url(ingredients) {
     const API_URL = `http://www.recipepuppy.com/api/?i=${Ingredients.serialize(ingredients)}`;
-    return API_URL;
+    return cors_proxy_url(API_URL);
 }
 
 var Ingredients = {
@@ -32,4 +32,16 @@ var Ingredients = {
     parse : function(ingredientString) {
         return ingredientString.split(',');
     }
+}
+
+// Helper function that 'wraps' URLs to go through a proxy
+// See: https://github.com/Rob--W/cors-anywhere
+// Basic Info on CORS: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+// 
+// Attention: Do this only for APIs you trust
+// (or, as in this case, for demonstration purposes)
+// There is a good reason why CORS restrictions exist:
+// https://www.pivotpointsecurity.com/blog/cross-origin-resource-sharing-security/
+function cors_proxy_url(url) {
+    return 'https://cors-anywhere.herokuapp.com/' + url;
 }
